2b3434463fd23c212c3499e773bebc51808c8853
Jaco Kok <jaco@dts.za.com>
2011-11-28 08:54:19 +0200

    API Test

---
+++ TestHarnessAPI/objectmap/objectmap.java
@@ -0,0 +1,35 @@
+package objectmap;
+import resources.objectmap.objectmapHelper;
+import com.rational.test.ft.*;
+import com.rational.test.ft.object.interfaces.*;
+import com.rational.test.ft.object.interfaces.SAP.*;
+import com.rational.test.ft.object.interfaces.WPF.*;
+import com.rational.test.ft.object.interfaces.dojo.*;
+import com.rational.test.ft.object.interfaces.siebel.*;
+import com.rational.test.ft.object.interfaces.flex.*;
+import com.rational.test.ft.object.interfaces.generichtmlsubdomain.*;
+import com.rational.test.ft.script.*;
+import com.rational.test.ft.value.*;
+import com.rational.test.ft.vp.*;
+import com.ibm.rational.test.ft.object.interfaces.sapwebportal.*;
+/**
+ * Description   : Functional Test Script
+ * @author Jaco
+ */
+public class objectmap extends objectmapHelper
+{
+	/**
+	 * Script Name   : <b>objectmap</b>
+	 * Generated     : <b>Nov 25, 2011 1:42:30 PM</b>
+	 * Description   : Functional Test Script
+	 * Original Host : WinNT Version 6.1  Build 7601 (S)
+	 * 
+	 * @since  2011/11/25
+	 * @author Jaco
+	 */
+	public void testMain(Object[] args) 
+	{
+		// TODO Insert code here
+	}
+}
+
